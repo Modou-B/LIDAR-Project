5d2171711fc8fefa72d3cf85e081d1451824e196
Momodou Berlemann <modou.be@gmail.com>
2022-11-09 09:24:11 +0100

    Expansion of documentation

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+'''info
+# titel: LIDAR-Room-Scnanner
+# date: 09.11.2022
+# autors: Momodou Berlamann, Julian Sommer
+
+# brief explanation:
+# This program will read and display the information gathered through a LIDAR-sensor combined, 
+# with servo engines to scan rooms and the objects that are in it. 
+'''
+
+'''documentation
+'''
+
 # main 
+...
