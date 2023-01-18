466916a1ebde9b642bc5e84a5503455bd99101b0
Julian155 <88496702+Julian155@users.noreply.github.com>
2023-01-18 01:22:05 +0100

    Refa

--- 3D-Room-Scanner.ino
+++ 3D-Room-Scanner.ino
@@ -2,23 +2,24 @@
 #include "TFMini.h"
 #include <Servo.h>
 
-const int minServo1MicroDegree = 544;
-const int minServo2MicroDegree = 1500;
+const double minHorizontalServoMicroDegree = 544; // get data tomorrow
+const double maxHorizontalServoMicroDegree = 2000; // get data tomorrow
+const double minVerticalServoMicroDegree = 800; // get data tomorrow
+const double maxVerticalServoMicroDegree = 1500; //get data tomorrow
 
-int servo1MicroDegree;
-int servo2MicroDegree;
+double horizontalMicroDegree;
+double verticalMicroDegree;
+double horizontalDegree;
+double verticalDegree;
 
-Servo servo1;
-Servo servo2;
-int DegreeServo1;
-int DegreeServo2;
+Servo horizontalServo;
+Servo verticalServo;
 
-int stop = 1;                               // 0 = Start script, 1 = Stop script
+int startScript = 0; // 1 = Start script, 0 = Stop script
 
+// Lidar port allocation
 TFMini tfmini;
-SoftwareSerial SerialTFMini(2, 0);          //The only value that matters here is the first one, 2, Rx
-
-
+SoftwareSerial SerialTFMini(2, 0);
 
 void getTFminiData(int* distance)
 {
@@ -27,14 +28,16 @@ void getTFminiData(int* distance)
   int checksum = 0;
   static int rx[9];
 
-  if (SerialTFMini.available())
-  {
+  if (SerialTFMini.available()) {
     rx[i] = SerialTFMini.read();
+
     if (rx[0] != 0x59) {
       i = 0;
-    } else if (i == 1 && rx[1] != 0x59) {
+    } 
+    else if (i == 1 && rx[1] != 0x59) {
       i = 0;
-    } else if (i == 8) {
+    } 
+    else if (i == 8) {
       for (j = 0; j < 8; j++) {
         checksum += rx[j];
       }
@@ -44,45 +47,41 @@ void getTFminiData(int* distance)
       }
 
       i = 0;
-    } else {
+    } 
+    else {
       i++;
     }
   }
 }
  
-void GetRange(int x_cord, int y_cord){
+void GetRange(double horizontalDegree, double verticalDegree)
+{
   int distance = 0;
- 
   getTFminiData(&distance);
-  while (!distance)
-  {
-    getTFminiData(&distance);
-    if (distance)
-    {
-      // Serial.println(distance);
 
+  while (!distance) {
+    getTFminiData(&distance);
+
+    if (distance) {
       Serial.print(distance);
       Serial.print(",");
-      Serial.print(x_cord);
+      Serial.print(horizontalDegree);
       Serial.print(",");
-      Serial.println(y_cord);
-
-
-      // Serial.print("cm\t");
+      Serial.println(verticalDegree);
     }
   }  
 }
- 
+
 void setup()
 {
   if (stop == 0) {
-    servo1.attach(9);
-    servo2.attach(10);
+    horizontalServo.attach(9);
+    verticalServo.attach(10);
     Serial.begin(9600);
     Serial.begin(115200);                   //Initialize hardware serial port (serial debug port)
 
     while (!Serial);                        // wait for serial port to connect. Needed for native USB port only
-    Serial.println ("Initializing...");
+    Serial.println("Initializing...");
     SerialTFMini.begin(TFMINI_BAUDRATE);    //Initialize the data rate for the SoftwareSerial port
     tfmini.begin(&SerialTFMini);            //Initialize the TF Mini sensor
   } 
@@ -90,67 +89,52 @@ void setup()
  
 void loop() 
 {
-  // --- write ---
-  servo2.write(100);
-  servo1.write(0);
+  
+  /* FIRST GET MICROSECONDS DATA TOMORROW
+  if (startScript) {
+    horizontalServo.write(0)
+    delay(400)
+    Serial.println("HORIZONTAL MIN MICROSECONDS = ",horizontalServo.readMicroSeconds());
+    delay(100)
+    horizontalServo.write(180)
+    delay(400)
+    Serial.println("HORIZONTAL MAX MICROSECONDS = ",horizontalServo.readMicroSeconds());
+    delay(100)
+    verticalServo.write(100)
+    delay(400)
+    Serial.println("VERTICAL MAX MICROSECONDS = ",verticalServo.readMicroSeconds());
+    delay(100)
+    verticalServo.write(30)
+    delay(400)
+    Serial.println("VERTICAL MIN MICROSECONDS = ",verticalServo.readMicroSeconds());
+    delay(100)
+  }
+  */
+
+
+  /*if (startScript) {  
+  // --- Set starting positions ---
+  verticalServo.writeMicroseconds(maxVerticalServoMicroDegree);
+  horizontalServo.writeMicroseconds(minHorizontalServoMicroDegree);
   delay((1000));
 
-  if (!stop) {  
-    for(int j = 100; j >= 30; j--) {
-      servo2.write(j);
-      servo1.write(0);
-      delay((200));
+  for(verticalMicroDegree = maxVerticalServoMicroDegree; verticalMicroDegree >= minVerticalServoMicroDegree; verticalMicroDegree -= 5) {
+    verticalServo.writeMicroseconds(verticalMicroDegree);
+    horizontalServo.writeMicroseconds(minHorizontalServoMicroDegree);
+    delay((200));
+    
+    for(horizontalMicroDegree = minHorizontalServoMicroDegree; horizontalMicroDegree <= maxHorizontalServoMicroDegree; horizontalMicroDegree += 5) {
+      horizontalDegree = (horizontalMicroDegree - minHorizontalServoMicroDegree) / 10;
+      verticalDegree = (verticalMicroDegree - minVerticalServoMicroDegree) / 10;
       
-      for(int i = 0; i <= 180; i++ ) {
-        GetRange(j,i);
-        servo1.write(i);
-        delay((4));
-      }
+      horizontalServo.writeMicroseconds(horizontalMicroDegree);
+      
+      delay((4));
+      GetRange(horizontalDegree, verticalDegree);
+      delay((2));
     }
-    servo2.write(100);
-    servo1.write(0);
-    stop = 1;
   }
-
-  // --- writeMicroseconds ---
-  // int y_cord = 101;
-  // int x_cord = 0;
-  // servo2.writeMicroseconds(1500);
-  // servo1.writeMicroseconds(544);
-  // delay((1000));
-
-  // if (!stop) {  
-  //   for(int j = 1500; j >= 800; j -=5) {
-  //     if (j < 800) {
-  //       j = 800;
-  //     }
-
-  //     if (y_cord > 30) {
-  //       y_cord--;
-  //     }
-
-  //     servo2.writeMicroseconds(j);
-  //     servo1.writeMicroseconds(0);
-  //     delay((200));
-  //     x_cord = 0;
-  //     for(int i = 544; i <= 2389; i +=5) {
-  //       if (i > 2389) {
-  //         i = 2389;
-  //       }
-
-  //       if (x_cord < 180) {
-  //         x_cord++; 
-  //       }
-
-  //       GetRange(y_cord,x_cord);
-  //       servo1.writeMicroseconds(i);
-  //       delay((4));
-  //     }
-  //   }
-  //   servo2.writeMicroseconds(1500);
-  //   servo1.writeMicroseconds(544);
-  //   stop = 1;
-  // }
-
-
+  
+  startScript = 0;
+  }*/
 }
