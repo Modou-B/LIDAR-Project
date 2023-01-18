7c9e0f06d99a9a204c52634d381460e844d13ebe
Julian155 <88496702+Julian155@users.noreply.github.com>
2023-01-18 01:34:57 +0100

    Add color depth to point cloud

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ import open3d as o3d
 
 # csv
 import csv
-    
 
 # initialize csv file
 f = open('path/to/csv_file', 'w')
@@ -37,13 +36,22 @@ time.sleep(1)
 # create Visualizer and Window
 vis = o3d.visualization.Visualizer()
 vis.create_window(height=640, width=860)
+
+# set render options
 visRenderOption = vis.get_render_option()
 visRenderOption.background_color = np.asarray([0,0,0])
+visRenderOption.point_size = 1.5
 
 # initialize PointCloud instance
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector([[0,0,140],[-20,-20,0]])
 vis.add_geometry(pcd)
+
+# initialize PointCloudcolor array with the 2 starting points
+pointCloudColors = [
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1]
+]
 
 # run non-blocking visualization
 keepRunning = True
@@ -60,6 +68,9 @@ while keepRunning:
         xAngle = int(splitPacket[2])
         yAngle = (int(splitPacket[1]) - 100) * -1
 
+        # set the depth of the white color dependent on the lidar distance
+        colorDepth = 1 - lidarDistance / 1000
+
         # write real world data to csv file for testing
         writer.writerow([xAngle, yAngle, lidarDistance])
 
@@ -68,19 +79,19 @@ while keepRunning:
         x = lidarDistance * np.cos(np.deg2rad(yAngle)) * np.cos(np.deg2rad(xAngle))
         z = lidarDistance * np.sin(np.deg2rad(yAngle))
 
-        # add point to cloud
-        numpyArray = np.asarray([[x,y,z]])
-        pcd.points.extend(o3d.utility.Vector3dVector(numpyArray))
-        pcd.paint_uniform_color([0.9, 0.9, 0.9])
-        print(lidarDistance,"cm"," X =",x,"Y =",y)
+        # add point with color to cloud
+        pointCloudColors.extend([[colorDepth,colorDepth,colorDepth]])
+        pcd.colors = o3d.utility.Vector3dVector(pointCloudColors)      
+        pcd.points.extend(o3d.utility.Vector3dVector([x,y,z]))
         
         vis.update_geometry(pcd)
+
+        print(lidarDistance,"cm"," X =",x,"Y =",y)
     
     keepRunning = vis.poll_events()
     vis.update_renderer()
 
 f.close()
-# vis.destroy_window
     
 
    
