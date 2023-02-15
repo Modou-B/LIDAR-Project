5a6adb04a4be3c9adde587d55864f428616a01cb
Julian155 <88496702+Julian155@users.noreply.github.com>
2023-02-15 01:18:07 +0100

    Keep Window open after arduino is finished

--- main.py
+++ main.py
@@ -28,22 +28,73 @@ visRenderOption = vis.get_render_option()
 visRenderOption.background_color = np.asarray([0,0,0])
 visRenderOption.point_size = 1.5
 
+startingPoints = [
+    [0,0,0],
+    [180,0,0],
+    [0,70,0],
+    [180,70,0],
+    [0,0,400],
+    [180,0,400],
+    [0,70,400],
+    [180,70,400]]
+
+convertedStartingPoints = []
+for startingPoint in startingPoints:
+    y = startingPoint[2] * np.cos(np.deg2rad(startingPoint[1])) * np.sin(np.deg2rad(startingPoint[0]))
+    x = startingPoint[2] * np.cos(np.deg2rad(startingPoint[1])) * np.cos(np.deg2rad(startingPoint[0]))
+    z = startingPoint[2] * np.sin(np.deg2rad(startingPoint[1]))
+    convertedStartingPoints.append([x,y,z])
+
 # initialize PointCloud instance
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector([[0,0,140],[-20,-20,0]])
+pcd.points = o3d.utility.Vector3dVector(convertedStartingPoints)
 vis.add_geometry(pcd)
 
-# initialize PointCloudcolor array with the 2 starting points
+# initialize PointCloudcolor array with the 8 starting points
 pointCloudColors = [
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1],
+    [0.1,0.1,0.1],
     [0.1,0.1,0.1],
     [0.1,0.1,0.1]
 ]
 
 # run non-blocking visualization
-while True:
+keepRunning = True
+while keepRunning:
     while (arduinoData.inWaiting()== 0):
-        pass
-    dataPacket = arduinoData.readline()
+        arduinoData.cancel_read()
+        y = 800 * np.cos(np.deg2rad(180)) * np.sin(np.deg2rad(180))
+        x = 800 * np.cos(np.deg2rad(180)) * np.cos(np.deg2rad(180))
+        z = 800 * np.sin(np.deg2rad(180))
+        pointCloudColors.extend([[0.1,0.1,0.1]])
+        pcd.colors = o3d.utility.Vector3dVector(pointCloudColors)      
+        pcd.points.extend(o3d.utility.Vector3dVector([[x,y,z]]))
+        vis.update_geometry(pcd)
+        keepRunning = vis.poll_events()
+
+        if keepRunning == False:
+            break
+
+        vis.update_renderer()
+        continue
+        
+    if keepRunning == False:
+        break
+        
+    time.sleep(.001)                  
+    dataPacket = arduinoData.readline()            
+    # check if full data is received. 
+    while not '\\n'in str(dataPacket):        
+    
+        time.sleep(.001)              
+        temp = arduinoData.readline()        
+        if not not temp.decode():     
+            dataPacket = (dataPacket.decode()+temp.decode()).encode()
+      
     dataPacket = str(dataPacket, 'utf-8')
     dataPacket = dataPacket.strip('\r\n')
     splitPacket = dataPacket.split(",")
@@ -53,7 +104,7 @@ while True:
             splitPacket[i] = '.'.join(splitPacket[i].split('.',-1)[:2])
             
             if splitPacket[i].startswith('.'):
-                splitPacket[i] = '0' + splitPacket[i]
+                continue
         
         lidarDistance = float(splitPacket[0])
         xAngle = float(splitPacket[2])
@@ -74,7 +125,7 @@ while True:
         
         vis.update_geometry(pcd)
 
-        print(lidarDistance,"cm"," X =",x,"Y =",y)
+        print("Y = ",y," X = ",x," Z = ",z)
         
-    vis.poll_events()
+    keepRunning = vis.poll_events()
     vis.update_renderer()
